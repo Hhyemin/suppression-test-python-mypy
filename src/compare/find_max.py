19b9ff4aa1ccd8e14dcb55ca4937271e1841b2fe
Huimin Hu <huhuimin236@gmail.com>
2023-08-07 18:10:11 +0200

    Adding 'misc' warning type to a existing suppression, result: 2 warning types in 1 line

--- src/compare/find_max.py
+++ src/compare/find_max.py
@@ -3,7 +3,7 @@ from typing import List
 # get the max number in a int list
 def find_max(numbers: List[int]) -> int:
     if not numbers:
-        return None  # type: ignore[return-value]
+        return None  # type: ignore[return-value, misc]
     return max(numbers)
 
 values2 = [95, 96, 97, 98, 99, 100]  
